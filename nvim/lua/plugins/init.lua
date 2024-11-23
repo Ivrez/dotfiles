046f3699a759c271c74226d49d5f5766f7b073de
ivrez <reznichivan@gmail.com>
2024-11-23 18:58:50 +0300

    nvim config

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = { enable = true },
+      })
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup()
+    end,
+  },
+  {
+    "shaunsingh/nord.nvim",
+    config = function()
+	theme = 'nord'
+    end,
+  },
+}
